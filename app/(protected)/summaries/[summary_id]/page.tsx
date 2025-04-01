36e0292a40a4de693138029d8d8b96e1f5627af9
DarshanchGIT <darshanchoudhary1511@gmail.com>
2025-04-01 21:03:40 +0530

    type errors

--- app/(protected)/summaries/[summary_id]/page.tsx
+++ app/(protected)/summaries/[summary_id]/page.tsx
@@ -3,34 +3,24 @@ import { IndividualSummaryComp } from "@/components/summaries/summaryId/summaryI
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-interface IndividualSummaryPageProps {
-  params: { summary_id: string };
-}
+export type ParamsType = Promise<{ summary_id: string }>;
 
 export default async function IndividualSummaryPage({
   params,
-}: IndividualSummaryPageProps) {
+}: {
+  params: ParamsType;
+}) {
+  const { summary_id } = await params;
+
   const { userId } = await auth();
   if (!userId) redirect("/sign-in");
 
-  // Ensure params is treated as an object
-  if (!params || typeof params !== "object" || !params.summary_id) {
-    console.error("Invalid params received.");
-    return (
-      <div className="text-2xl font-medium text-red-500 text-center flex h-screen justify-center items-center">
-        Invalid page parameters.
-      </div>
-    );
-  }
-
-  const { summary_id } = params;
   const response = await getSummaryById(summary_id);
-
   if (!response.summary) {
-    console.error("No summary found for this ID");
+    console.error("No summary for this ID");
     return (
       <div className="text-2xl font-medium text-red-500 text-center flex h-screen justify-center items-center">
-        Failed to fetch summary, try again later.
+        Failed to fetch summary, try again sometime
       </div>
     );
   }
