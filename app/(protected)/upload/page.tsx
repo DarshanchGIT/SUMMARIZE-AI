398032776fae5773395f800f81ecaf7e9a65eb28
DarshanchGIT <darshanchoudhary1511@gmail.com>
2025-04-03 16:33:57 +0530

    Removed unnecessary page render

--- app/(protected)/upload/page.tsx
+++ app/(protected)/upload/page.tsx
@@ -1,11 +1,9 @@
-import { UploadPage } from "@/components/upload/upload-page";
+import { UploadDiv } from "@/components/upload/input-form";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
-    const { userId } = await auth()
-    if (!userId) redirect('/')
-    return (
-        <UploadPage />
-    );
+  const { userId } = await auth();
+  if (!userId) redirect("/");
+  return <UploadDiv />;
 }
